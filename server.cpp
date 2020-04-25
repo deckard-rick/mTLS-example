64b3053cf71550fe33c12c5852e553be4868c173
Andreas Tengicki <tengicki@autopoll.de>
2020-04-25 20:31:36 +0200

    SocketServer

--- server.cpp
+++ server.cpp
@@ -9,14 +9,19 @@
 #include <string.h>
 #define PORT 8080
 
-int main(int argc, char const *argv[])
+class ClearingServerSocket {
+public:
+  void setup(sockaddr_in *t_address);
+  void loop();
+private:
+  int server_fd;
+  struct sockaddr_in *address;
+};
+
+void ClearingServerSocket::setup(sockaddr_in *t_address)
 {
-	int server_fd, new_socket, valread;
-	struct sockaddr_in address;
+  address = t_address;
 	int opt = 1;
-	int addrlen = sizeof(address);
-	char buffer[1024] = {0};
-	char hello[] = "Hello from server";
 
 	// Creating socket file descriptor
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -33,12 +38,8 @@ int main(int argc, char const *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-  address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons( PORT );
-
 	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+	if (bind(server_fd, (struct sockaddr *)address, sizeof(*address))<0)
 	{
 		perror("bind failed");
 		exit(EXIT_FAILURE);
@@ -48,6 +49,15 @@ int main(int argc, char const *argv[])
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
+}
+
+void ClearingServerSocket::loop()
+{
+  int valread = 0;
+  char buffer[1024] = {0};
+  char hello[] = "Hello from server";
+  int new_socket = 0;
+  int addrlen = sizeof(address);
 
   if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
 					(socklen_t*)&addrlen))<0)
@@ -60,6 +70,19 @@ int main(int argc, char const *argv[])
 	printf("%s\n",buffer );
 	send(new_socket , hello , strlen(hello) , 0 );
 	printf("Hello message sent\n");
+}
+
+int main(int argc, char const *argv[])
+{
+  ClearingServerSocket socket;
+
+  struct sockaddr_in address;
+  address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons( PORT );
+
+  socket.setup(&address);
+  socket.loop();
 
   return 0;
 }
