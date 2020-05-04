91f38170da86ae390d17b423c9794178fed71fea
Andreas Tengicki <tengicki@autopoll.de>
2020-05-04 09:03:42 +0200

    Server mit SSL

--- server.cpp
+++ server.cpp
@@ -8,10 +8,24 @@
 //Step 3
 //Threading
 //https://dzone.com/articles/parallel-tcpip-socket-server-with-multi-threading
+//https://gist.github.com/oleksiiBobko/43d33b3c25c03bcc9b2b
 
 //Step 4
 //Das mit der Klasse ist eine Fehlentwicklung, das macht es in dem kleinen Beispiel nur
 //komplex, wieder raus, und dann schauen wir mal.
+
+//Step 5
+//SSL
+// ein einfaches Beispiel was nicht tut, vermutlich OpenSSL 1.0.2 vs 1.1.0
+//   https://wiki.openssl.org/index.php/Simple_TLS_Server
+// etwa unverständlich
+//   https://quuxplusone.github.io/blog/2020/01/24/openssl-part-1/
+// Sourcen aus github geladen, da sind demos drin.
+//   https://github.com/openssl/openssl
+// Über die Erzeugung von Schlüsseln
+//   https://www.grund-wissen.de/linux/server/openssl.html
+// Hier wird das Zertifikat abgefragt
+//  http://fm4dd.com/openssl/sslconnect.shtm
 
 #include <unistd.h>
 #include <stdio.h>
@@ -20,22 +34,75 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <pthread.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 #define PORT 8080
+#define MAXFILENAME 63
 
-void socket_setup(sockaddr_in *address, int &server_fd)
+class serverContext
+{
+  public:
+    bool withSSL = true;
+    SSL_CTX *ctx = NULL;
+    char cert[MAXFILENAME+1] = "cert.pem";
+    char key[MAXFILENAME+1] = "key.pem";
+    int serverSocket = -1;
+};
+
+class connectionContext
+{
+  public:
+    int socket = -1;
+    serverContext *sCtx = NULL;
+};
+
+SSL_CTX *create_context()
+{
+    SSL_CTX *ctx;
+
+    ctx = SSL_CTX_new(TLS_server_method());
+    if (!ctx)
+      {
+	      perror("Unable to create SSL context");
+	      ERR_print_errors_fp(stderr);
+	      exit(EXIT_FAILURE);
+      }
+
+    return ctx;
+}
+
+void configure_context(serverContext *srvCtx)
+{
+    SSL_CTX_set_ecdh_auto(srvCtx->ctx, 1);
+
+    /* Set the key and cert */
+    if (SSL_CTX_use_certificate_file(srvCtx->ctx, srvCtx->cert, SSL_FILETYPE_PEM) <= 0)
+      {
+        ERR_print_errors_fp(stderr);
+	      exit(EXIT_FAILURE);
+      }
+
+    if (SSL_CTX_use_PrivateKey_file(srvCtx->ctx, srvCtx->key, SSL_FILETYPE_PEM) <= 0 )
+      {
+        ERR_print_errors_fp(stderr);
+	      exit(EXIT_FAILURE);
+      }
+}
+
+void socket_setup(sockaddr_in *address, serverContext *srvCtx)
 {
 	int opt = 1;
 
 	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	if ((srvCtx->serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
 	{
 		perror("socket failed");
 		exit(EXIT_FAILURE);
 	}
 
 	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+	if (setsockopt(srvCtx->serverSocket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
 												&opt, sizeof(opt)))
 	{
 		perror("setsockopt");
@@ -43,12 +110,12 @@ void socket_setup(sockaddr_in *address, int &server_fd)
 	}
 
 	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr *)address, sizeof(*address))<0)
+	if (bind(srvCtx->serverSocket, (struct sockaddr *)address, sizeof(*address))<0)
 	{
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
-	if (listen(server_fd, 3) < 0)
+	if (listen(srvCtx->serverSocket, 3) < 0)
 	{
 		perror("listen");
 		exit(EXIT_FAILURE);
@@ -58,21 +125,53 @@ void socket_setup(sockaddr_in *address, int &server_fd)
 void *connection_handler(void *socket_desc)
 {
   //Get the socket descriptor
-   int sock = *(int*)socket_desc;
+   connectionContext *cc = (connectionContext*)socket_desc;
+
+   printf("Beginn connection handling.\n");
+
    char buffer[1024];
-   char hello[] = "Hello from server";
+   char hello[] = "Hello from server via SSL\n";
 
-   int valread = read(sock , buffer, 1024);
-   //WARNING: Das beide einfach auf printf rausrotzen ist natürlich nichts.
- 	 printf("%s\n",buffer );
+   SSL *ssl = NULL;
+   if (cc->sCtx->withSSL)
+    {
+       ssl = SSL_new(cc->sCtx->ctx);
+       if (ssl == NULL)
+         ERR_print_errors_fp(stderr);
 
-   send(sock , hello , strlen(hello) , 0 );
- 	 printf("Hello message sent\n");
+       if (!SSL_set_fd(ssl, cc->socket))
+         ERR_print_errors_fp(stderr);
+
+       if (SSL_accept(ssl) <= 0)
+         {
+           ERR_print_errors_fp(stderr);
+         }
+     }
+
+   if (cc->sCtx->withSSL)
+     SSL_read( ssl, buffer, 1024);
+   else
+     read(cc->socket , buffer, 1024);
+   printf("out: %s\n",buffer);
+
+   if (cc->sCtx->withSSL)
+      SSL_write(ssl, hello, strlen(hello));
+   else
+      send(cc->socket , hello , strlen(hello) , 0);
+ 	 printf("Hello server message sent\n");
+
+   if (cc->sCtx->withSSL)
+     {
+       SSL_shutdown(ssl);
+       SSL_free(ssl);
+     }
+
+   close(cc->socket);
 
    pthread_exit(NULL);
 }
 
-void socket_loop(sockaddr_in *address, int server_fd)
+void socket_loop(sockaddr_in *address, serverContext *srvCtx)
 {
   int valread = 0;
   char buffer[1024] = {0};
@@ -80,16 +179,23 @@ void socket_loop(sockaddr_in *address, int server_fd)
   int addrlen = sizeof(address);
   pthread_t thread_id;
 
-  while( client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen) )
-      {
-          printf("Connection accepted");
+  printf("Waiting for socket!\n");
 
-          if( pthread_create(&thread_id , NULL ,  connection_handler, (void*) &client_socket) < 0)
-          {
-              printf("could not create thread");
-          }
-          printf("Handler assigned");
+  while( client_socket = accept(srvCtx->serverSocket, (struct sockaddr *)&address, (socklen_t*)&addrlen) )
+      {
+          printf("Connection accepted\n");
+
+          connectionContext* cc = new connectionContext();
+          cc->sCtx = srvCtx;
+          cc->socket = client_socket;
+
+          if( pthread_create(&thread_id , NULL ,  connection_handler, (void*)cc) < 0)
+            {
+              printf("could not create thread\n");
+            }
       }
+
+  printf("End of socket_loop\n");
 
   pthread_exit(NULL);
 }
@@ -101,10 +207,27 @@ int main(int argc, char const *argv[])
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons( PORT );
 
-  int server_fd;
+  serverContext *srvCtx = new serverContext();
 
-  socket_setup(&address,server_fd);
-  socket_loop(&address,server_fd);
+  for (int i=0; i<argc; ++i)
+    {
+      if (strcmp(argv[i],"-plain")==0)
+        srvCtx->withSSL = false;
+      else if ((strcmp(argv[i],"-cert")==0) && (i+1 < argc) && (strlen(argv[i+1]) > 0))
+        strncpy(srvCtx->cert,argv[i+1],MAXFILENAME);
+      else if ((strcmp(argv[i],"-key")==0) && (i+1 < argc) && (strlen(argv[i+1]) > 0))
+        strncpy(srvCtx->key,argv[i+1],MAXFILENAME);
+    }
+
+  if (srvCtx->withSSL = true)
+    {
+      srvCtx->ctx = create_context();
+      configure_context(srvCtx);
+      printf("mit SSL\n");
+    }
+
+  socket_setup(&address,srvCtx);
+  socket_loop(&address,srvCtx);
 
   return 0;
 }
